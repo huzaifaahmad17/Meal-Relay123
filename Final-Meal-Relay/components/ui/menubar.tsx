e959003e2b437671d7e5192161bd7f1aba3337eb
Geetika223 <geetikachauhan141@gmail.com>
2026-09-25 04:39:22 +0530

    Export Menubar components for external usage

--- Final-Meal-Relay/components/ui/menubar.tsx
+++ Final-Meal-Relay/components/ui/menubar.tsx
@@ -216,4 +216,21 @@ const MenubarShortcut = ({
 };
 MenubarShortcut.displayname = 'MenubarShortcut';
 
-
+export {
+  Menubar,
+  MenubarMenu,
+  MenubarTrigger,
+  MenubarContent,
+  MenubarItem,
+  MenubarSeparator,
+  MenubarLabel,
+  MenubarCheckboxItem,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarPortal,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarGroup,
+  MenubarSub,
+  MenubarShortcut,
+};
