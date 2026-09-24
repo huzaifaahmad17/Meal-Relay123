dbf4d7a7c327818765ef3f9b98e50f97a3cc0c08
Lamecod <silentishu16@gmail.com>
2026-09-25 04:50:55 +0530

    Delete RootLayout component from layout.tsx
    Removed RootLayout component and its associated props.

--- Final-Meal-Relay/app/layout.tsx
+++ Final-Meal-Relay/app/layout.tsx
@@ -34,19 +34,4 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} antialiased`}>
-        <ThemeProvider>
-          {children}
-          <Toaster richColors position="top-right" />
-        </ThemeProvider>
-      </body>
-    </html>
-  );
-}
+
