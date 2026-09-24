7f6aaab7b8dff64d6c1fb87b48f722ba696f49ba
huzaifaahmad17 <huzaifa.mfp@gmail.com>
2026-09-25 04:47:12 +0530

    Remove ButtonProps interface from button component
    Removed ButtonProps interface definition from button.tsx.

--- Final-Meal-Relay/components/ui/button.tsx
+++ Final-Meal-Relay/components/ui/button.tsx
@@ -33,11 +33,6 @@ const buttonVariants = cva(
   }
 );
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
-}
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
